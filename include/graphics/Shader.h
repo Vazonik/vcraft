6542fc454e18cc51146750c174cbf5591b2749a0
Dawid Draguła <dawid.dragula@gmail.com>
2021-02-27 09:00:47 +0100

    Shader class

--- include/graphics/Shader.h
+++ include/graphics/Shader.h
@@ -2,6 +2,7 @@
 #define VCRAFT_SHADER_H
 
 #include <glad/glad.h>
+#include <iostream>
 
 #include "utilities/viewproj.h"
 
@@ -9,7 +10,6 @@ namespace vc {
     class Shader {
     public:
         enum Type {
-            full,
             vertex,
             fragment
         };
@@ -20,9 +20,25 @@ namespace vc {
         };
     public:
         Shader(char *vsPath, char *fsPath, size_t n, struct VertexAttr attributes[]);
-        [[nodiscard]] GLint getHandle(Type type = Type::full) const;
+        [[nodiscard]] GLint getHandle() const;
+        [[nodiscard]] GLint getHandle(Type type) const;
+    private:
+        static GLuint compile(char *path, Type type);
+        static void checkCompileErrors(GLint shader, char* path, vc::Shader::Type type);
     private:
         GLint handle{}, vsHandle{}, fsHandle{};
+    public:
+        void use() const;
+        void destroy() const;
+        void sendViewProj(ViewProj vp) const;
+        void sendMat4(const std::string &name, glm::mat4 m) const;
+        void sendTexture2D(); /* TODO: Send texture 2D to shader */
+        void sendFloat(const std::string &name, float f) const;
+        void sendVec2(const std::string &name, glm::vec2 v) const;
+        void sendVec3(const std::string &name, glm::vec3 v) const;
+        void sendVec4(const std::string &name, glm::vec4 v) const;
+        void sendInt(const std::string &name, int i) const;
+        void sentUInt(const std::string &name, unsigned int ui) const;
     };
 }
 
--- src/graphics/Shader.cpp
+++ src/graphics/Shader.cpp
@@ -1,19 +1,151 @@
 
 #include <graphics/Shader.h>
+#include <cstdio>
+#include <fstream>
+#include <sstream>
 
 vc::Shader::Shader(char *vsPath, char *fsPath, size_t n, vc::Shader::VertexAttr *attributes) {
+    vsHandle = compile(vsPath, Type::vertex);
+    fsHandle = compile(fsPath, Type::fragment);
+    handle = glCreateProgram();
+    glAttachShader(handle, vsHandle);
+    glAttachShader(handle, fsHandle);
 
+    for (size_t i = 0; i < n; i++) {
+        glBindAttribLocation(handle, attributes[i].index, attributes[i].name);
+    }
+
+    glLinkProgram(handle);
+
+    // Check link status
+    GLint linked;
+    glGetProgramiv(handle, GL_LINK_STATUS, &linked);
+
+    if (linked == 0) {
+        char buf[512];
+        snprintf(buf, 512, "[%s, %s]", vsPath, fsPath);
+        std::cout << "[Error] Linking shader at " << buf << " failed." << std::endl;
+    }
 }
 
+GLuint vc::Shader::compile(char* path, vc::Shader::Type type) {
+    std::string strCode;
+    std::ifstream file;
+
+    file.exceptions(std::fstream::failbit | std::fstream::badbit);
+
+    try {
+        file.open(path);
+        std::stringstream ss;
+        ss << file.rdbuf();
+        file.close();
+        strCode = ss.str();
+    } catch (std::ifstream::failure &e) {
+        std::cout << "Shader at " << path << " read failed." << std::endl;
+    }
+
+    const char *code = strCode.c_str();
+    GLuint handle;
+
+    switch(type) {
+        case Type::vertex:
+            handle = glCreateShader(GL_VERTEX_SHADER);
+            break;
+        case Type::fragment:
+            handle = glCreateShader(GL_FRAGMENT_SHADER);
+            break;
+    }
+
+    glShaderSource(handle, 1, &code, nullptr);
+    glCompileShader(handle);
+    checkCompileErrors(handle, path, type);
+
+    return handle;
+}
+
+void vc::Shader::checkCompileErrors(GLint shader, char *path, vc::Shader::Type type) {
+    int success;
+    char infoLog[1024];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+
+        std::string typeName;
+        switch(type) {
+            case vc::Shader::Type::fragment:
+                typeName = "Fragment shader";
+                break;
+            case vc::Shader::Type::vertex:
+                typeName = "Vertex shader";
+                break;
+            default:
+                typeName = "Shader";
+        }
+
+        std::cout << "[ERROR] " << typeName << " at " << path << " compilation error:" << std::endl;
+        std::cout << infoLog << std::endl;
+    }
+}
 
 // getters
+GLint vc::Shader::getHandle() const {
+    return handle;
+}
+
 GLint vc::Shader::getHandle(vc::Shader::Type type) const {
     switch(type) {
         case Type::fragment:
             return fsHandle;
         case Type::vertex:
             return vsHandle;
-        default:
-            return handle;
     }
+}
+
+void vc::Shader::use() const {
+    glUseProgram(handle);
+}
+
+void vc::Shader::destroy() const {
+    glDeleteProgram(handle);
+    glDeleteShader(vsHandle);
+    glDeleteShader(fsHandle);
+}
+
+
+// send (uniforms)
+void vc::Shader::sendViewProj(ViewProj vp) const {
+    sendMat4("p", vp.proj);
+    sendMat4("v", vp.view);
+}
+
+void vc::Shader::sendMat4(const std::string &name, glm::mat4 m) const {
+    glUniformMatrix4fv(glGetUniformLocation(handle, name.c_str()), 1, GL_FALSE, &m[0][0]);
+}
+
+void vc::Shader::sendTexture2D() {
+    // for later
+}
+
+void vc::Shader::sendFloat(const std::string &name, float f) const {
+    glUniform1f(glGetUniformLocation(handle, name.c_str()), f);
+}
+
+void vc::Shader::sendVec2(const std::string &name, glm::vec2 v) const {
+    glUniform2f(glGetUniformLocation(handle, name.c_str()), v.x, v.y);
+}
+
+void vc::Shader::sendVec3(const std::string &name, glm::vec3 v) const {
+    glUniform3f(glGetUniformLocation(handle, name.c_str()), v.x, v.y, v.z);
+}
+
+void vc::Shader::sendVec4(const std::string &name, glm::vec4 v) const {
+    glUniform4f(glGetUniformLocation(handle, name.c_str()), v.x, v.y, v.z, v.w);
+}
+
+void vc::Shader::sendInt(const std::string &name, int i) const {
+    glUniform1i(glGetUniformLocation(handle, name.c_str()), i);
+}
+
+void vc::Shader::sentUInt(const std::string &name, unsigned int ui) const {
+    glUniform1ui(glGetUniformLocation(handle, name.c_str()), ui);
 }
